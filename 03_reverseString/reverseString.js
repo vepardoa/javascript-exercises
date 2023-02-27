092952c60c52eada287e29b1b053a9fef671d614
vepardoa <violetaepardo@gmail.com>
2023-02-27 00:08:13 -0600

    Add exercise 03 solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedStr = '';
+    const Arr = string.split("");
 
+    for (let i = Arr.length-1; i>=0; i--){
+        reversedStr += Arr[i];
+    }
+    return reversedStr;
 };
 
 // Do not edit below this line
