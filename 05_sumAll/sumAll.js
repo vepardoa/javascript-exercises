d6c2cb99d589660648ab8a101513f2430039a9e7
vepardoa <violetaepardo@gmail.com>
2023-02-27 01:28:25 -0600

    Add exercise 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
 
+    if (typeof(n1) != 'number'|| typeof(n2) != 'number') return "ERROR";
+    if (n1<0 || n2<0 ) return "ERROR";
+    
+    let sum = 0;
+    if(n1>n2){
+        max = n1;
+        min = n2;
+    }
+    else{
+        max = n2;
+        min = n1;
+    } 
+    for(let i=min; i<=max; i++) sum += i;
+    return sum;
 };
 
 // Do not edit below this line
