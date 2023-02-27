2459ec2393dbd227a5256c2c65fbb9444338ae45
vepardoa <violetaepardo@gmail.com>
2023-02-27 01:10:01 -0600

    Add exercise 04 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
+    const refArray = [];
+
+    array.forEach((element) => {
+        if(!args.includes(element)){
+            refArray.push(element);
+        }
+    });
+    
+
+    return refArray;
 };
 
 // Do not edit below this line
